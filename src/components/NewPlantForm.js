580a4fbe3a0741849d67447b7c4a74ed5d61f9ba
Bryant Wiggins <topwigit@gmail.com>
2023-05-31 20:26:00 -0400

    solution again

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({plants, setPlants}) {
+
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    const newPlant = {
+      name: e.target.name.value,
+      image:e.target.image.value,
+      price:e.target.price.value
+    }
+
+    fetch('http://localhost:6001/plants', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPlant)
+})
+   .then(response => response.json())
+   .then((newPlant) => setPlants([...plants, newPlant]))
+
+  }
+
+  
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((resp) => resp.json())
+    .then((data) => setPlants(data))
+  }, [])
+
+  //My use state now that we have to begin rendering
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredPlants = plants.filter((plant) => (
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ))
+
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants}/>
+      <Search setSearchTerm={setSearchTerm}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
